5f483c8974bf545469af52ac0e00a83c9b3351e9
theethawat <theethawat.s@outlook.com>
2023-05-10 14:34:24 +0700

    ✨ Update หน้า Insert pallet

--- src/components/BottomNav.jsx
+++ src/components/BottomNav.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function BottomNav() {
   return (
@@ -23,28 +24,16 @@ function BottomNav() {
               หน้าแรก
             </span>
           </Link>
-          <button
+          <Link
+            to='/insert'
             type='button'
             className='inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group'
           >
-            <svg
-              className='w-6 h-6 mb-1 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500'
-              fill='currentColor'
-              viewBox='0 0 20 20'
-              xmlns='http://www.w3.org/2000/svg'
-              aria-hidden='true'
-            >
-              <path d='M4 4a2 2 0 00-2 2v1h16V6a2 2 0 00-2-2H4z'></path>
-              <path
-                clip-rule='evenodd'
-                fill-rule='evenodd'
-                d='M18 9H2v5a2 2 0 002 2h12a2 2 0 002-2V9zM4 13a1 1 0 011-1h1a1 1 0 110 2H5a1 1 0 01-1-1zm5-1a1 1 0 100 2h1a1 1 0 100-2H9z'
-              ></path>
-            </svg>
+            <FontAwesomeIcon icon='fa-solid fa-pallet' />
             <span className='text-sm text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500'>
               ใส่พาเลท
             </span>
-          </button>
+          </Link>
           <button
             type='button'
             className='inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group'
--- src/views/InsertPallets.js
+++ src/views/InsertPallets.js
@@ -1,9 +1,25 @@
-import React from 'react'
-
+import React from "react";
+import Layout from "../components/Layout";
+import { Controller, useForm } from "react-hook-form";
+import { Input } from "@mui/joy";
 function InsertPallets() {
+  const { control } = useForm();
   return (
-    <div>InsertPallets</div>
-  )
+    <Layout title='ใส่พาเลทไปยังคลัง'>
+      <form>
+        <div className='flex flex-wrap'>
+          <div className='w-full py-1 p-2'>สแกนคลัง</div>
+          <Controller
+            control={control}
+            name='warehouse'
+            render={({ field }) => (
+              <Input {...field} placeholder='คลังสินค้า' fullWidth autoFocus />
+            )}
+          />
+        </div>
+      </form>
+    </Layout>
+  );
 }
 
-export default InsertPallets
+export default InsertPallets;
